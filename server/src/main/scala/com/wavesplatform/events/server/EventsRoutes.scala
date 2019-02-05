b72063191b1aacaa1b88918eab03a2b556841878
vgavrilin <yoba123@yandex.ru>
2019-02-05 17:18:16 +0300

    Server fixes

--- server/src/main/scala/com/wavesplatform/events/server/EventsRoutes.scala
+++ server/src/main/scala/com/wavesplatform/events/server/EventsRoutes.scala
@@ -10,29 +10,29 @@ import akka.stream.scaladsl.Source
 import akka.stream.typed.scaladsl.ActorSource
 import akka.stream.{Materializer, OverflowStrategy}
 import com.wavesplatform.events.client.actors.NodeTxFeed
+import play.api.libs.json.Json
 
 import scala.language.postfixOps
 
-class EventsRoutes(implicit as: ActorSystem, mat: Materializer) extends Directives {
+class EventsRoutes(feed: ActorRef[NodeTxFeed.Message])(implicit as: ActorSystem, mat: Materializer) extends Directives {
   private[this] type SSEStream = Source[ServerSentEvent, NotUsed]
-
-  private[this] def getStream(actor: ActorRef[NodeTxFeed.Message], subscription: NodeTxFeed.Subscription): SSEStream = {
-    ActorSource.actorRef[NodeTxFeed.Transactions](PartialFunction.empty, PartialFunction.empty, 128, OverflowStrategy.dropHead)
-      .mapConcat(_.tx.toVector)
-      .map(_ => ServerSentEvent(???, ???))
-      .mapMaterializedValue(actor ! NodeTxFeed.Subscribe(_, subscription))
-      .mapMaterializedValue(_ => NotUsed)
-  }
-
 
   val ssePaths: Route = path("transactions") {
     path("address" / Base58Address) { address =>
-      complete(getStream(???, NodeTxFeed.Subscription.Address(address)))
+      complete(getJsonStream(NodeTxFeed.Subscription.Address(address)))
     } ~
     path("data-key" / Segment) { dataKey =>
-      complete(getStream(???, NodeTxFeed.Subscription.DataKey(dataKey)))
+      complete(getJsonStream(NodeTxFeed.Subscription.DataKey(dataKey)))
     }
   }
 
   val routes: Route = get(ssePaths)
+
+  private[this] def getJsonStream(subscription: NodeTxFeed.Subscription): SSEStream = {
+    ActorSource.actorRef[NodeTxFeed.Transactions](PartialFunction.empty, PartialFunction.empty, 128, OverflowStrategy.dropHead)
+      .mapConcat(_.tx.toVector)
+      .map(tx => ServerSentEvent(Json.stringify(tx.json())))
+      .mapMaterializedValue(feed ! NodeTxFeed.Subscribe(_, subscription))
+      .mapMaterializedValue(_ => NotUsed)
+  }
 }
